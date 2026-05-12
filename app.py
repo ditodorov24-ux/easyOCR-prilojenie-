e2ed0053e5eafa5dfe31fdb30415272e9ab80348
ditodorov24-ux <ditodorov24@gmail.com>
2026-05-12 14:13:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ from PIL import Image
 import numpy as np
 import re
 
-# Заглавие
 st.title("🧾 Food Scanner")
 
-# OCR loader
+# OCR
 @st.cache_resource
 def load_reader():
     return easyocr.Reader(['en'], gpu=False)
@@ -30,14 +29,14 @@ uploaded_file = st.file_uploader(
     type=["jpg", "jpeg", "png"]
 )
 
-# Ако има снимка
+# Само ако има снимка
 if uploaded_file is not None:
 
-    # Отваряне
+    # Отваряне на изображението
     image = Image.open(uploaded_file).convert("RGB")
 
     # Показване
-    st.image(image, width=300)
+    st.image(image)
 
     # Convert към numpy
     img_np = np.array(image)
@@ -46,50 +45,50 @@ if uploaded_file is not None:
     with st.spinner("Scanning..."):
         results = reader.readtext(img_np)
 
-    # Текст
-    extracted_text = " ".join([x[1] for x in results])
+    # Извличане на текст
+    extracted_text = ""
 
-    st.subheader("OCR Текст")
+    for r in results:
+        extracted_text += r[1] + " "
+
+    # Показване
+    st.subheader("OCR Text")
     st.write(extracted_text)
 
-    # ------------------------
-    # ПОЧИСТВАНЕ НА ТЕКСТА
-    # ------------------------
+    # Почистване
     clean_text = extracted_text.lower()
 
     clean_text = re.sub(
-        r'[\s\-,.:;()]',
+        r'[^a-zA-Z0-9]',
         '',
         clean_text
     )
 
-    st.subheader("Почистен текст")
+    st.subheader("Clean Text")
     st.write(clean_text)
 
-    # ------------------------
-    # ТЪРСЕНЕ
-    # ------------------------
+    # Търсене
     found = []
 
-    for ingredient, description in harmful_ingredients.items():
+    for ingredient in harmful_ingredients:
 
-        ingredient_clean = ingredient.replace(" ", "").lower()
+        ingredient_clean = ingredient.lower()
 
         if ingredient_clean in clean_text:
 
             found.append(
-                f"{ingredient.upper()} → {description}"
+                ingredient.upper()
+                + " → "
+                + harmful_ingredients[ingredient]
             )
 
-    # ------------------------
-    # РЕЗУЛТАТ
-    # ------------------------
-    st.subheader("Резултат")
+    # Резултат
+    st.subheader("Result")
 
-    if found:
+    if len(found) > 0:
 
         for item in found:
             st.error(item)
 
     else:
-        st.success("Няма открити вредни съставки")
+        st.success("No harmful ingredients found")
